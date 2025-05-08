485c237512a94790c14c22683a41abf3adf4c5f2
Israela608 <israela608@gmail.com>
2025-05-08 16:55:06 +0100

    Sign up screen, custom notification dialog

--- lib/common/plain_text_field.dart
+++ lib/common/plain_text_field.dart
@@ -1,7 +1,6 @@
 import 'package:finance_news/core/utils/app_colors.dart';
 import 'package:finance_news/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PlainTextField extends StatelessWidget {
--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -24,11 +24,20 @@ class AppColor {
   static const Color muted50 = Color(0xFFFAFAFA);
   static const Color muted300 = Color(0xFFD4D4D4);
 
-  // Error Colors
-  static const Color errorBase400 = Color(0xFFD42620);
+  // Success
+  static const Color success50 = Color(0xFFE7F6EC);
+  static const Color success75 = Color(0xFFB5E3C4);
+  static const Color successBase400 = Color(0xFF0F973D);
+  static const Color success500 = Color(0xFF099137);
+
+  // Error
+  static const Color error50 = Color(0xFFFBEAE9);
+  static const Color error75 = Color(0xFFF2BCBA);
   static const Color error500 = Color(0xFFCB1A14);
-  static const Color error600 = Color(0xFFBA110B);
-  static const Color error700 = Color(0xFF9E0A05);
-  static const Color error800 = Color(0xFF800501);
-  static const Color error900 = Color(0xFF800501);
+
+// Grey
+  static const Color grey200 = Color(0xFFE4E7EC);
+  static const Color grey600 = Color(0xFF475367);
+
+  static const Color grey900 = Color(0xFF101928);
 }
