d93aa4bd9a63ca3e42cdd500e956193544ed5ac0
Israela608 <israela608@gmail.com>
2025-05-09 08:29:29 +0100

    Added API key to safe config file

---
+++ lib/data/constants/api_constants.dart
@@ -0,0 +1,9 @@
+import 'package:finance_news/data/constants/config.dart';
+
+class ApiConstants {
+  ApiConstants._();
+
+  static const String baseUrl = 'https://finnhub.io/api/v1';
+  static const String newsUrI = '/news?category=general';
+  static const String token = API_KEY;
+}
